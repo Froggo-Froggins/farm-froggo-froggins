d4a82592746fcf6d42f78510a1951c67ee7fe6b6
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-05-12 12:36:14 +0200

    small updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "$",
+  title: "FROGO FROGGINS",
   description: "EASIEST 100x COIN YET!",
 };
 
